fd89970f1bf4cc0a62a3274a4c5219662e05a08a
hansxf-ui <ryanmorelas25@gmail.com>
2026-09-26 23:53:05 +0800

    Add files via upload

--- app/idol/[name]/page.js
+++ app/idol/[name]/page.js
@@ -8,7 +8,7 @@ import ThemeToggle from '../../components/ThemeToggle'
 import Lightbox from '../../components/Lightbox'
 import Skeleton from '../../components/Skeleton'
 
-const thumb = (it) => (it.type === 'youtube' ? `https://img.youtube.com/vi/${ytId(it.url)}/hqdefault.jpg` : it.url)
+const thumb = (it) => (it.type === 'youtube' ? `https://img.youtube.com/vi/${ytId(it.url)}/mqdefault.jpg` : it.url)
 
 export default function IdolAlbum() {
   const { name } = useParams()
