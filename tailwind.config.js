981548dd3321e6d00edf86969e9ff1d6cdf6a1d9
hansxf-ui <ryanmorelas25@gmail.com>
2026-09-26 21:37:49 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   darkMode: 'class',
   content: ['./app/**/*.{js,jsx}'],
   theme: { extend: {
-    colors: { milk: '#FFF3F7', rose: '#F7A8C8', lilac: '#CDB8F5', gold: '#C9A24B', plum: '#3B2140' },
+    // Palet "biru-putih-pink" Hearts2Hearts (nama token dipertahankan supaya className lama tetap jalan):
+    // milk = putih sejuk, rose = biru langit, lilac = biru es, gold = pink, plum = biru laut dalam
+    colors: { milk: '#F5FAFE', rose: '#7FC4E8', lilac: '#C9E6F7', gold: '#EA93BB', plum: '#173A56' },
     fontFamily: { display: ['"Playfair Display"', 'serif'], body: ['Quicksand', 'sans-serif'] }
   } },
   plugins: []
